9da2d86cb4588569c0d8c81386346652fe2e74dd
Jesús Mira <jesmira@hotmail.es>
2023-05-07 12:19:29 +0200

    Control props alternativo

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -6,35 +6,20 @@ export const useShoppingCart = () => {
     const [shoppingCart, setShoppingCart] = useState< {[key:string]: ProductInCart }>({});
 
     const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
-      // shoppingCart[ product.id] = {...product, count}
-  
-  
+      // shoppingCart[ product.id] = {...product, count} 
       setShoppingCart( oldShoppingCart => {
-        
-        const ProductInCart : ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0}
-  
-        if( Math.max( ProductInCart.count + count, 0) > 0){
-          ProductInCart.count += count;
-          return {
-              ...oldShoppingCart,
-              [product.id]:ProductInCart
-          }
-        }
-  
-        // Borrar el producto (toDelete esta preparado para avisar al usuario, por eso marca que no se ha usado)
+          
+        if (count === 0 ){
+          // toDelete esta preparado para avisar al usuario, por eso marca que no se ha usado
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
           const { [product.id]: toDelete, ...rest  } = oldShoppingCart;
           return rest;
+        }
   
-        // if (count === 0 ){
-        //   // toDelete esta preparado para avisar al usuario, por eso marca que no se ha usado
-        //   const { [product.id]: toDelete, ...rest  } = oldShoppingCart;
-        //   return rest;
-        // }
-  
-        // return {
-        //   ...oldShoppingCart,
-        //   [product.id]:{ ...product, count }
-        // }
+        return {
+          ...oldShoppingCart,
+          [product.id]:{ ...product, count }
+        }
       })
       
     }
